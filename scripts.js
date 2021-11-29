c9e060e1a2fbd034d2f0be6e66e3cc6fa976e41b
Marcos Querino <mqjunioretecgv@gmail.com>
2021-11-29 20:04:11 -0300

    reservas: +botão e +funções
    + novo botão no html para cálculo de diárias, que usa a função verifyDiárias()
    + nova função que devolve condição normal ao input da partida. é a arrumaCor()
    + nova função para arrumar número de crianças, é a verify5anos()

--- scripts.js
+++ scripts.js
@@ -27,6 +27,30 @@ function verifyCidade() {
     alert("Cidade inválida");
 }
 
+function verifyDiárias() {
+  var chegada = new Date(document.getElementById("chegada").value);
+  var partida = new Date(document.getElementById("partida").value);
+  if (partida > chegada) {
+    var dias = (partida.getTime() - chegada.getTime());
+    var dias = dias /  (1000 * 60 * 60 * 24);
+    document.getElementById("diária").innerHTML = "Sua reserva tem " + dias + " diária(s).";
+  }
+  else {
+    alert("A data de chegada é maior que a data de partida");
+    document.getElementById("partida").style.backgroundColor = "yellow";
+  }
+}
+
+function arrumaCor() {
+  document.getElementById("partida").style.backgroundColor = "white";
+  document.getElementById("partida").value = null;
+}
+
+function verify5anos() {
+  if (document.getElementById("bebês").value > document.getElementById("crianças").value)
+    document.getElementById("bebês").value = document.getElementById("crianças").value;
+}
+
 function confirmaEnvio() {
   alert('Obrigado! Em breve, entraremos em contato para confirmação de sua reserva.');
 }
