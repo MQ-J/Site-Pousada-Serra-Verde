a199513f149a7447dbd25b4e2e09019a7522e3fd
Marcos Querino <mqjunioretecgv@gmail.com>
2021-11-28 23:52:09 -0300

    adicionando funções

--- scripts.js
+++ scripts.js
@@ -3,19 +3,28 @@ function verifyNome() {
   var numb = /[0-9]/;
   var espaco = / /;
   if (numb.test(campo) || !espaco.test(campo))
-    alert("Ta errado aqui");
+    alert("Somente nome e sobrenome");
 }
 
 function verifyCel() {
-  
+  var letra = /[a-zA-Z]/;
+  if (letra.test(document.getElementById("celular").value))
+    alert("Celular inválido");
 }
 
 function verifyEmail() {
-  
+  var campo = document.getElementById("email").value;
+  var arroba = /@/;
+  var ponto = /\./;
+  if (!arroba.test(campo) || !ponto.test(campo))
+    alert("Email inválido");
 }
 
 function verifyCidade() {
-  
+  var campo = document.getElementById("cidade").value;
+  var numb = /[0-9]/;
+  if (numb.test(campo))
+    alert("Cidade inválida");
 }
 
 function confirmaEnvio() {
